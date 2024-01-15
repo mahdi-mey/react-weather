7627d4ce1c47671ffdfc44a72a4ceac604640b72
Mahdi <mahdimey17@gmail.com>
2024-01-15 23:47:48 +0330

    added page structure with location state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import React from "react";
 class App extends React.Component {
 
     constructor(props) {
-        super()
+        super(props)
+
+        this.state = {
+            location: ''
+        }
     }
 
 
     render(){
         return(
             <div className="app">
-            
+                <h1>Weather App</h1>
+                <div>
+                    <input type="text" placeholder="Search for location"  onChange={e => this.setState({location: e.target.value})} />
+                </div>
+                <button>Get Weather</button>
             </div>
         )
     }
