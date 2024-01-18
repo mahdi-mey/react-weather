23ef8e009b57a81d151172a65f2b12e09fbc5708
Mahdi <mahdimey17@gmail.com>
2024-01-18 18:14:46 +0330

    sent data over to Day component

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,21 @@
 import React from "react";
+import Day from "./Day";
 
 class Weather extends React.Component {
     render() {
-        const {temperature_2m_max: max, temperature_2m_min: min, time: date, weathercode: codes} = this.props.weather
+        const {temperature_2m_max: max, temperature_2m_min: min, time: dates, weathercode: codes} = this.props.weather
 
 
         return (
             <div>
                 <h2>Weather in {this.props.location}</h2>
+                <ul className="weather">
+                    {
+                        dates.map((date, i) => (
+                            <Day date={date} max={max[i]} min={min[i]} codes={codes[i]} key={date} />
+                        ))
+                    }
+                </ul>
             </div>
         )
     }
